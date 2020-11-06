89dd8f0fe5c37660bede9f17a4b1256141532afb
DarKiiii <lucasd0104@gmail.com>
2020-11-06 12:31:37 +0100

    update

--- db.js
+++ db.js
@@ -25,6 +25,7 @@ module.exports = {
         return table;
     },
     get: function (table_name, row, column) {
+		if (!fs.existsSync('./db/' + table_name + '.json')) fs.writeFileSync('./db/' + table_name + '.json', "{}");
         let table = JSON.parse(fs.readFileSync('./db/' + table_name + '.json'));
         if (!table[row]) return false;
         if (!table[row][column]) return false;
