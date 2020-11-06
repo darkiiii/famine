e340c1aa07fa60f90818625b06f7d81f8d625132
DarKiiii <52256277+DarKiiii@users.noreply.github.com>
2020-11-06 12:41:56 +0100

    Update prefix.js

--- cmds/prefix.js
+++ cmds/prefix.js
@@ -5,13 +5,13 @@ module.exports.run = async (bot, message, args) => {
     if (!args[0]) {
         msg = await message.channel.send("Le nouveau préfixe est invalide, il n'a pas été changé");
         message.delete();
-        msg.delete({timeout: 3000});
+        msg.delete({timeout: 5000});
         return;
     };
     db.set("guilds", message.guild.id, "prefix", args[0]);
     msg = await message.channel.send("Le préfixe a été changé pour : **" + args[0] + "**");
     message.delete();
-    msg.delete({timeout: 3000});
+    msg.delete({timeout: 5000});
 }
 
 module.exports.help = {
