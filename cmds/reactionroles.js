865a677b06b85d2fd4b4c16c25c6c759884fad8d
DarKiiii <52256277+DarKiiii@users.noreply.github.com>
2020-11-06 12:41:42 +0100

    Update reactionroles.js

--- cmds/reactionroles.js
+++ cmds/reactionroles.js
@@ -2,7 +2,11 @@
 const db = require('../db.js');
 
 module.exports.run = async (bot, message, args) => {
-    if (!args[3]) return message.channel.send("Le nombre d'arguments est invalide (min: 4)");
+    if (!args[3]) {
+        msg = await message.channel.send("Le nombre d'arguments est invalide (min: 4)");
+        msg.delete({timeout:5000});
+        return;
+    }
     await message.channel.messages.fetch(args[0]).then(Rmsg => {
         Rmsg.react(args[1]);
         let args_f = args.filter(function (value, index, arr) { return value != ""; });
