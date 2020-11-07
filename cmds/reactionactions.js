8f6acd2259d406cd65a632b3362271794887cd2f
DarKiiii <lucasd0104@gmail.com>
2020-11-07 15:35:01 +0100

    remove node_modules

--- cmds/reactionactions.js
+++ cmds/reactionactions.js
@@ -5,9 +5,10 @@ module.exports.run = async (bot, message, args) => {
     if (!args[3]) {
         msg = await message.channel.send("Le nombre d'arguments est invalide (min: 4)");
         msg.delete({timeout:5000});
+        message.delete();
         return;
     }
-    await message.channel.messages.fetch(args[0]).then(Rmsg => {
+    await message.channel.messages.fetch(args[0]).then( async Rmsg => {
         Rmsg.react(args[1]);
         let args_f = args.filter(function (value, index, arr) { return value != ""; });
 
@@ -18,6 +19,9 @@ module.exports.run = async (bot, message, args) => {
             reactJson[args_f[1].toString()] = JSON.parse(db.resolve("{\"action\": \"" + args_f[2].toString() + "\",\"action_args\": [\"" + args_f.slice(3).join("\",\"").toString() + "\"]}"));
         }
         db.set("reactionactions", Rmsg.channel.id, Rmsg.id, reactJson);
+        msg = await message.channel.send("La réaction " + args_f[1].toString() + " du message " + Rmsg.id.toString() + " donnera le/les roles " + args_f.slice(3).join(", ").toString());
+        msg.delete({timeout:5000});
+        message.delete();
     }).catch(err => {
         message.channel.send("Le bot a rencontre une erreur : ```" + err + "```");
         console.log(err);
@@ -26,5 +30,5 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
     name: "reactionactions",
-	aliases: ["ra"]
+	aliases: ["ract", "raction", "ractions", "reactionaction"]
 }
